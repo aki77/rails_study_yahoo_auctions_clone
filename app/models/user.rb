f672e0f67321336c4df34fb86f4f4cd32a83a3f8
aki <lala.akira@gmail.com>
2015-12-21 13:37:46 +0900

    ユーザ登録・ログイン・ログアウトリンクを設置

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,38 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :integer          not null, primary key
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  sign_in_count          :integer          default(0), not null
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :string
+#  last_sign_in_ip        :string
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  username               :string           default(""), not null
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#  index_users_on_username              (username) UNIQUE
+#
+
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  VALID_USERNAME_REGEX = /\A[a-z]\w*[a-z0-9]\z/i
+  validates :username, presence: true,
+                       format: { with: VALID_USERNAME_REGEX },
+                       uniqueness: { case_sensitive: false },
+                       length: { maximum: 15 }
 end
