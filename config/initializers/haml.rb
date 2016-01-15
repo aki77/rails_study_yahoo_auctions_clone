e8924a89bc9b506a75a1b32a68199e6017de762a
aki <lala.akira@gmail.com>
2016-01-15 14:25:15 +0900

    xss脆弱性の修正

--- config/initializers/haml.rb
+++ config/initializers/haml.rb
@@ -1,5 +1,2 @@
-# http://qiita.com/kami_zh/items/913a8cfd1ddc2052b1c8
-Haml::Template.options = {
-  attr_wrapper: '"',  # タグの属性値を「"」で囲む（デフォルトは「'」）
-  ugly: true,         # インデント等の扱いを各環境下で統一する
-}
+Haml::Template.options[:attr_wrapper] = '"'
+Haml::Template.options[:ugly] = true
